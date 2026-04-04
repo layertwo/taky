0d37f8b58677f82c63653a107e2eae44cf6a3ede
Lucas Messenger <1335960+layertwo@users.noreply.github.com>
2026-04-04 13:58:40 -0700

    refactor(models): convert GeoChat to independent frozen dataclass

--- taky/cot/models/geochat.py
+++ taky/cot/models/geochat.py
@@ -1,9 +1,10 @@
 import enum
 import xml.etree.ElementTree as etree
+from dataclasses import dataclass
+from datetime import datetime
 
 from dateutil.parser import isoparse
 
-from taky.cot.models.detail import Detail
 from taky.cot.models.errors import UnmarshalError
 from taky.cot.models.teams import Teams
 
@@ -16,7 +17,8 @@ class ChatParents(enum.Enum):
     TEAM = "TeamGroups"
 
 
-class GeoChat(Detail):
+@dataclass(frozen=True, repr=False)
+class GeoChat:
     """
     Class representing a GeoChat message
 
@@ -25,25 +27,16 @@ class GeoChat(Detail):
     attempts to unify the field names and meanings.
     """
 
-    def __init__(self, elm):
-        super().__init__(elm)
-
-        self.chatroom = None  # detail/__chat.chatroom
-        self.chat_parent = None  # detail/__chat.parent
-        self.group_owner = False  # detail/__chat.groupOwner
-
-        # We get these fields explicitly
-        self.src_uid = None  # detail/link.uid
-        self.src_cs = None  # detail/__chat.senderCallsign
-        self.src_marker = None  # detail/link.type
-        self.message = None  # detail/remarks
-        self.message_ts = None  # detail/remarks.time
-
-        # These fields are inferred
-        # The UID (string) of the recipient (if an individual user, otherwise None)
-        self.dst_uid = None
-        # cot.Teams (if a group message, otherwise None)
-        self.dst_team = None
+    chatroom: str | None = None
+    chat_parent: str | None = None
+    group_owner: bool = False
+    src_uid: str | None = None
+    src_cs: str | None = None
+    src_marker: str | None = None
+    message: str | None = None
+    message_ts: datetime | None = None
+    dst_uid: str | None = None
+    dst_team: Teams | None = None
 
     def __repr__(self):
         if self.broadcast:
@@ -75,8 +68,8 @@ class GeoChat(Detail):
     def is_type(tags):
         return GEOCHAT_TAGS.issubset(tags)
 
-    @staticmethod
-    def from_elm(elm):
+    @classmethod
+    def from_elm(cls, elm):
         if elm.tag != "detail":
             raise UnmarshalError("Cannot create GeoChat from %s" % elm.tag)
 
@@ -90,31 +83,30 @@ class GeoChat(Detail):
         if None in [chat, chatgrp, remarks, link]:
             raise UnmarshalError("Detail does not contain GeoChat")
 
-        gch = GeoChat(elm)
-        gch.chat_parent = chat.get("parent")
-        gch.group_owner = chat.get("groupOwner") == "true"
-        gch.src_uid = link.get("uid")
-        gch.src_cs = chat.get("senderCallsign")
-        gch.src_marker = link.get("type")
+        chatroom = chat.get("chatroom")
+        chat_parent = chat.get("parent")
+        dst_uid = None
+        dst_team = None
+        if chat_parent == ChatParents.TEAM.value:
+            dst_team = Teams(chatroom)
+        elif chatroom != ALL_CHAT_ROOMS:
+            dst_uid = chat.get("id")
 
-        gch.chatroom = chat.get("chatroom")
-        if gch.chat_parent == ChatParents.TEAM.value:
-            gch.dst_team = Teams(gch.chatroom)
-        elif gch.chatroom != ALL_CHAT_ROOMS:
-            # Router will have to fill out .dst
-            gch.dst_uid = chat.get("id")
-
-        gch.message = remarks.text
-        gch.message_ts = isoparse(remarks.get("time")).replace(tzinfo=None)
-
-        return gch
+        return cls(
+            chatroom=chatroom,
+            chat_parent=chat_parent,
+            group_owner=chat.get("groupOwner") == "true",
+            src_uid=link.get("uid"),
+            src_cs=chat.get("senderCallsign"),
+            src_marker=link.get("type"),
+            message=remarks.text,
+            message_ts=isoparse(remarks.get("time")).replace(tzinfo=None),
+            dst_uid=dst_uid,
+            dst_team=dst_team,
+        )
 
     @property
     def as_element(self):
-        if self.elm is not None:
-            return self.elm
-
-        # Fill in hacky destination UID
         if self.broadcast:
             dst_uid = ALL_CHAT_ROOMS
         elif self.dst_team:
@@ -123,7 +115,6 @@ class GeoChat(Detail):
             dst_uid = self.dst_uid
 
         detail = etree.Element("detail")
-        # id is optional for All Chat Rooms?
         chat = etree.Element(
             "__chat",
             attrib={
